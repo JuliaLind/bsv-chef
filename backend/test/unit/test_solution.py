c82f378dfe56fdf7a4f1e2b96d667fe3f4821a68
Julia Lind <juli22@student.bth.se>
2024-05-20 18:06:23 +0200

    added tests for test_solution

--- backend/test/unit/test_solution.py
+++ backend/test/unit/test_solution.py
@@ -1,6 +1,50 @@
 import pytest
+from unittest.mock import MagicMock
+from unittest.mock import patch
+from src.controllers.recipecontroller import RecipeController
 
-# add your test case implementation here
+
+@pytest.fixture
+def sut():
+    mocked_items_dao = MagicMock()
+    sut = RecipeController(mocked_items_dao)
+    return sut
+
+
+three_recipes = {
+    "mid ready recipe": 0.7,
+    "least ready recipe": 0.65,
+    "most ready recipe": 0.75
+}
+
 @pytest.mark.unit
-def test():
-    pass
+@pytest.mark.parametrize(
+    'recipes, take_best, expected_outcome',
+    [
+        ({}, False, None),
+        ({'single recipe': 0.1}, False, 'single recipe'),
+        (three_recipes, True, 'most ready recipe'),
+        (three_recipes, False, 'mid ready recipe'),
+    ])
+def test_get_recipe(sut, recipes, take_best,expected_outcome):
+    """
+    Tests the get_recipe method. This test includes
+    4 parametrized test cases:
+    1. there are no available recipes, should return None
+    2. there is one available recipe, the recipe should be returned
+    3. there are three available recipes with readiness values
+       0.7, 0.65 and 0.75, and take_best is set to True.
+       The recipe with readiness 0.75, named 'most ready recipe', should be the one returned.
+    4. there are three available recipes with readiness values
+       0.7, 0.65 and 0.75 and take_best is set to False.
+       The recipe with readiness 0.7, named 'mid ready recipe', should be the one returned
+    """
+
+    user_diet = MagicMock()
+
+    with patch('src.controllers.recipecontroller.RecipeController.get_readiness_of_recipes', return_value = recipes), \
+        patch('random.randint', return_value = 1):
+        result =  sut.get_recipe(user_diet, take_best)
+
+
+        assert result == expected_outcome
